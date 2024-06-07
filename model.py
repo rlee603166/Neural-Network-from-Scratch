c63137be07e5bf97be3edb06983a0f0ae26bbc20
Ryan Lee <rylee603166@gmail.com>
2024-06-07 06:07:53 -0700

    Finished all code, and model is 100% complete

--- model.py
+++ model.py
@@ -8,25 +8,27 @@ class NeuralNetwork:
         self.learning_rate = 0
         self.seed = np.random.seed(42)
     
-    def label_encoder(y):
+    def label_encoder(self, y):
         zeros = np.zeros((y.shape[0], np.max(y)+1))
         for i, num in enumerate(y):
             zeros[i][np.squeeze(num)] = 1
         return zeros.T
 
-    def relu(Z):
+    def relu(self, Z):
         return np.maximum(0,Z)
     
-    def sigmoid(Z):
+    def sigmoid(self, Z):
         sigmoid = 1 / (1 + np.exp(-Z))
         return sigmoid
     
-    def softmax(Z):
+    def softmax(self, Z):
         softmax = np.exp(Z) / sum(np.exp(Z))
         return softmax
+    
+    def relu_deriv(self, Z):
+        return Z > 0
 
-    def initialize_parameters(n_x, n_h=10, n_y=10): # (number of inputs, number of units in hidden, number of outputs)
-    # Need 2 sets of parameters for 2-layer network
+    def initialize_parameters(self, n_x, n_h=10, n_y=10):
         W1 = np.random.randn(n_h, n_x) * 0.01
         b1 = np.zeros((n_h, 1))
         W2 = np.random.randn(n_y, n_h) * 0.01
@@ -58,7 +60,7 @@ class NeuralNetwork:
         return A2
         
     # Back prop method 
-    def backward_propagation(self, X, y):
+    def back_propagation(self, X, y):
         W1, b1, W2, b2 = self.parameters.values()
         Z1, A1, Z2, A2 = self.cache.values()
     
@@ -81,36 +83,35 @@ class NeuralNetwork:
             'db1': db1
         }
     
-    def update_parameters(self, learning_rate):
+    def update_parameters(self):
         W1 = self.parameters['W1'].copy()
         b1 = self.parameters['b1'].copy()
         W2 = self.parameters['W2'].copy()
         b2 = self.parameters['b2'].copy()
         
         self.parameters['W1'] = W1 - self.learning_rate * self.gradients['dW1']
-        self.parameters['b1'] = W1 - self.learning_rate * self.gradients['db1']
-        self.parameters['W2'] = W1 - self.learning_rate * self.gradients['dW2']
-        self.parameters['b2'] = W1 - self.learning_rate * self.gradients['db2']
+        self.parameters['b1'] = b1 - self.learning_rate * self.gradients['db1']
+        self.parameters['W2'] = W2 - self.learning_rate * self.gradients['dW2']
+        self.parameters['b2'] = b2 - self.learning_rate * self.gradients['db2']
 
     
     # Methods for results
-    def get_predictions(A2):
+    def get_predictions(self, A2):
         return np.argmax(A2,0)
 
-    def get_accuracy(predictions, Y):
+    def get_accuracy(self, predictions, Y):
         print(predictions, Y)
         return np.sum(predictions == Y) / Y.size
     
-    def fit(self, X, y, learning_rate = 0.1, num_iterations = 3000):
+    def fit(self, X, y, learning_rate = 0.1, num_iterations = 1000):
         self.learning_rate = learning_rate
-        self.parameters = self.initialize_parameters(X.shape[0], 10, 10)
-        print(self.parameters)
+        self.parameters = self.initialize_parameters(X.shape[0])
         for i in range(num_iterations):  
             A2 = self.forward_propagation(X)
             self.back_propagation(X, y)
             self.update_parameters()
             
-            if i % 10 == 0:
+            if i % 100 == 0:
                 print(f'Accuracy for iteration {i}: {self.get_accuracy(self.get_predictions(A2), y)}')
 
     # Methods for dev/test sets
